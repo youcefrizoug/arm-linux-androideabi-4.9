93f8146973961ea883995f02f7fa920ddeacecd6
root <root@localhost>
2025-06-24 16:23:03 +0000

    test

---
+++ lib/gcc/arm-linux-androideabi/4.9.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../src/gcc-4.9.4/configure --target=arm-linux-androideabi --prefix=/root/toolchain-arm/install --with-sysroot=/opt/ubuntu --enable-languages=c --without-isl --disable-nls --without-headers --disable-shared --disable-threads --disable-multilib --disable-libssp --disable-libmudflap --disable-libgomp --disable-libquadmath --disable-decimal-float --with-newlib --with-gnu-as --with-gnu-ld --with-as=/root/toolchain-arm/install/bin/arm-linux-androideabi-as --with-ld=/root/toolchain-arm/install/bin/arm-linux-androideabi-ld";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "tls", "gnu" } };
